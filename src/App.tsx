b993de65e41c7af30dde34ba2e40008234e37d5e
Limon <ali.aliev.27.11.02@gmail.com>
2022-07-31 19:52:59 +0600

    Create project, Add Input component, Add some css, Make some layout

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import {Input} from "./components/input/Input";
+import {useState} from "react";
+
+
+function App() {
+  const todoListJson = localStorage.getItem("todoList");
+  let todoList: string[] = [];
+  let [tasks, setTasks] = useState(['']);
+  if (todoListJson) {
+    todoList = JSON.parse(todoListJson);
+    tasks = todoList;
+  }
+  console.log(() => todoList);
+  const someFun = (value: string) => {
+    todoList.push(value);
+    setTasks(todoList);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    console.log(JSON.stringify(todoList));
+
+  }
+
+  const view = () => tasks
+    .map((str: string, i: number) => <li key={i + str} className='task'>{str}
+      <div className='task-btns'>
+        <button className='edit-btn'>
+
+        </button>
+        <button className='delete-btn'>x</button>
+      </div>
+    </li>)
+    .reverse();
+
+  return (
+    <div className="App">
+      <header className='header'>
+        <a href="#" className='logo-link'>
+          <img src={reactLogo} alt="react logo" className="logo"/>
+          <h1 className='title'>Ali Express To Do List</h1>
+        </a>
+      </header>
+      <div>
+        <Input handleClick={someFun}/>
+      </div>
+      <ul className='task-list'>
+        {view()}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import './Input.css'
+
+export const Input = ({handleClick}: any) => {
+  const [task, setTask] = useState("");
+  const handleChange = (event: any) => {
+    setTask(event.target.value);
+  };
+
+  return (
+    <div className='input-div'>
+      <input
+        type="text"
+        id="task"
+        name="task"
+        onChange={handleChange}
+        value={task}
+        className='input'/>
+      <button onClick={() => handleClick(task)}>Add task</button>
+    </div>
+  )
+}
